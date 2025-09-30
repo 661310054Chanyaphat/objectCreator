73027e11f69c76f1e3cc3b609a31d6747fd26226
Chanyaphat661310054 <pream.aoy@gmail.com>
2025-09-30 20:27:22 +0700

    init repo

---
+++ objectCreatorUtil.py
@@ -0,0 +1,24 @@
+# objectCreatorUtil.py
+import maya.cmds as cmds
+
+def create_object(obj_type: str, name: str = None):
+    
+    obj_map = {
+        "cube": cmds.polyCube,
+        "cone": cmds.polyCone,
+        "sphere": cmds.polySphere,
+        "torus": cmds.polyTorus,
+    }
+
+    if obj_type not in obj_map:
+        cmds.warning(f"Unknown object type: {obj_type}")
+        return None
+
+    create_func = obj_map[obj_type]
+    obj, _ = create_func()
+
+    
+    if name:
+        obj = cmds.rename(obj, name)
+
+    return obj
